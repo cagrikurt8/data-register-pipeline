4e512b17c75d71bee2775572e63819f1234f2f40
cagrikurt8 <cagri.kurt8@outlook.com>
2023-03-23 14:26:22 +0300

    update

--- src/script_folder/data-register.py
+++ src/script_folder/data-register.py
@@ -1,29 +1,19 @@
-from azureml.core import Workspace, Dataset
+from azureml.core import Workspace, Dataset, Run, Datastore
 import argparse
 
 
-def main(args):
-    path = args.data_path
-    ws = Workspace.from_config()
-    dataset = Dataset.Tabular.from_delimited_files(path=path)
-    dataset.register(ws, 'sample-data')
+def main():
+    ws = Workspace(subscription_id="bc387bcb-42e5-4ef4-9d60-8c36a83b5778",
+                   resource_group="AI-ML_RG",
+                   workspace_name="aml-workspace")
 
-
-def parse_args():
-    # setup arg parser
-    parser = argparse.ArgumentParser()
-
-    # add arguments
-    parser.add_argument("--data_path", dest='data_path',
-                        type=str)
-
-    # parse args
-    args = parser.parse_args()
-
-    # return args
-    return args
+    ds = Datastore.get(ws, 'analyticsblob')
+    dataset = Dataset.Tabular.from_delimited_files(path=(ds, 'data/salesdata.csv'),
+                                                   header='ALL_FILES_HAVE_SAME_HEADERS')
+    dataset.register(workspace=ws, 
+                    name='sample-data',
+                    create_new_version=True)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main()
